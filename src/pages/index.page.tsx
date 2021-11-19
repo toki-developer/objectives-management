cb10b1a4d944b73c592c719e8d6c39b1fa0fc231
Toki <55911995+tokio-k@users.noreply.github.com>
2021-11-19 18:13:05 +0900

    Setup auth

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,25 +1,29 @@
-import { gql } from "@apollo/client";
-import { useMyQueryQuery } from "src/apollo/graphql";
+import { supabase } from "src/utils/libs/initSupabase";
 
 const Home = () => {
-  const { data, loading, error } = useMyQueryQuery();
-  console.log(data);
-  console.log(loading);
-  console.log(error);
+  const handleSignUp = () => {
+    supabase.auth.signUp({ provider: "google" });
+  };
+  const handleSignIn = () => {
+    supabase.auth.signIn({ provider: "google" });
+  };
+  const handleSignOut = () => {
+    supabase.auth.signOut();
+  };
   return (
     <div>
       <h1>Hello World</h1>
+      <div>
+        <button onClick={handleSignUp}>sign up</button>
+      </div>
+      <div>
+        <button onClick={handleSignIn}>sign in</button>
+      </div>
+      <div>
+        <button onClick={handleSignOut}>sign out</button>
+      </div>
     </div>
   );
 };
 
 export default Home;
-
-gql`
-  query MyQuery {
-    objectives {
-      id
-      title
-    }
-  }
-`;
