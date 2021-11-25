0645ab4397ed20f3c2cb3a15bf3e8a3f088e8512
Toki <55911995+tokio-k@users.noreply.github.com>
2021-11-25 22:50:06 +0900

    Setting auth redirect

--- src/utils/hooks/useAuth.ts
+++ src/utils/hooks/useAuth.ts
@@ -2,7 +2,12 @@ import { supabase } from "src/utils/libs/initSupabase";
 
 export const useAuth = () => {
   const handleSignIn = () => {
-    supabase.auth.signIn({ provider: "google" });
+    supabase.auth.signIn(
+      { provider: "google" },
+      {
+        redirectTo: window.location.origin,
+      }
+    );
   };
   const handleSignOut = () => {
     supabase.auth.signOut();
