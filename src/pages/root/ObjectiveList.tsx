d5b8b83cdb5d6774d2d835763c1fa063ed2828fd
Toki <55911995+tokio-k@users.noreply.github.com>
2021-11-28 22:06:32 +0900

    Add skelton loading

--- src/pages/root/ObjectiveList.tsx
+++ src/pages/root/ObjectiveList.tsx
@@ -2,9 +2,9 @@ import { gql, useReactiveVar } from "@apollo/client";
 import { userVar } from "src/apollo/cache";
 import type { ObjectiveFieldFragment } from "src/apollo/graphql";
 import { useGetObjectiveListQuery } from "src/apollo/graphql";
-import { LoadingIcon } from "src/components/Icon";
 import { RequireLoginMessage } from "src/components/Message";
 import { Objective } from "src/pages/root/Objective";
+import { SkeletonObjective } from "src/pages/root/SkeletonObjective";
 
 const EXAMPLE_OBJECTIVE: ObjectiveFieldFragment = {
   id: 0,
@@ -13,7 +13,12 @@ const EXAMPLE_OBJECTIVE: ObjectiveFieldFragment = {
     { id: 0, title: "目的のため", items_type: 1, evaluation_type: 0 },
     { id: 0, title: "行動をする", items_type: 2, evaluation_type: 0 },
     { id: 0, title: "行動をする", items_type: 2, evaluation_type: 0 },
-    { id: 0, title: "評価指標を達成できたかどうか", items_type: 3, evaluation_type: 0 },
+    {
+      id: 0,
+      title: "評価指標を達成できたかどうか",
+      items_type: 3,
+      evaluation_type: 0,
+    },
   ],
 };
 
@@ -28,8 +33,10 @@ const NotLoggedObjective = () => {
 
 export const ObjectiveList = () => {
   const loginUser = useReactiveVar(userVar);
-  const { data, loading, error } = useGetObjectiveListQuery({ skip: !loginUser });
-  if (loading) return <LoadingIcon />;
+  const { data, loading, error } = useGetObjectiveListQuery({
+    skip: !loginUser,
+  });
+  if (loading) return <SkeletonObjective />;
   if (error) return <p>error</p>;
   if (!loginUser) {
     return <NotLoggedObjective />;
