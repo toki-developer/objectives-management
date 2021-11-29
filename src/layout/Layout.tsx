ecb6968d77404ec58d217b04e8d9d7a4c6649f37
Toki <55911995+tokio-k@users.noreply.github.com>
2021-11-29 21:58:43 +0900

    Add Header

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout: VFC<Props> = (props) => {
   // eslint-disable-next-line react/destructuring-assignment
   const { children } = props;
   return (
-    <div className="max-w-4xl mx-auto w-full px-4">
+    <div className="max-w-4xl mx-auto w-full px-4 space-y-4">
       <Header />
       <main>{children}</main>
     </div>
